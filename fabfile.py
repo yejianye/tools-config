40f5e12fd1baf586bd8f2ce5ef42311c4bfc7024
Ryan Ye <yejianye@gmail.com>
2012-09-30 21:45:39 +0800

    recipe for installing wordpress with nginx

--- fabfile.py
+++ fabfile.py
@@ -144,11 +144,18 @@ def wordpress_nginx():
 	#[ensure_package(pkg) for pkg in pkgs]
 	sed('/etc/php5/fpm/php.ini', '^;? *cgi.fix_pathinfo *= *1', 'cgi.fix_pathinfo=0', use_sudo=True)
 	upload_template('wp_blog', '/etc/nginx/sites-available/wp_blog', use_sudo=True,
-		context={'server_name' : '113.11.199.44'})	
+		context={'server_name' : 'ryanye.me, blog.ryanye.me, www.ryanye.me'})	
 	with cd('/etc/nginx/sites-enabled/'):
 		sudo('ln -sf ../sites-available/wp_blog')
 	sudo('service nginx restart')
-	sudo('bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost')
+	sudo('bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress blog.ryanye.me')
+	# fix uploads directory permission issue
+	sudo('chown www-data:www-data -R /srv/www/wp-uploads')
+	# set default language to Chinese in wp-config
+	print "To allow direct plugin/content upload, add the following line to /etc/wordpress/confg-localhost.php"
+	print "    define('FS_METHOD', 'direct');"
+	print "To change default language to Chinese,"
+	print "    define('WPLANG', 'zh_CN');"
 
 @task
 def all():
